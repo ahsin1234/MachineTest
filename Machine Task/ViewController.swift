126277b74d7e52f945cc9626f6069ae44f33ccd2
Ahsin <ahsin.ibrar@hotmail.com>
2020-06-02 15:50:33 +0400

    Initial Commit

---
+++ Machine Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine Task
+//
+//  Created by MacBookPro on 02/06/2020.
+//  Copyright © 2020 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
